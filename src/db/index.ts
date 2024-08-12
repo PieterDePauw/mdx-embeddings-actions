1d875182a32520ab0174f5ffcc3da9ba0cabed43
Pieter <pieter@pieterdepauw.com>
2024-08-12 22:55:33 +0200

    chore: Update import paths to match new directory structure

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,14 @@
 // Import modules
-import { sql } from "@vercel/postgres"
-import { drizzle } from "drizzle-orm/vercel-postgres"
+// import { sql /* db */ } from "@vercel/postgres"
+// import { drizzle } from "drizzle-orm/vercel-postgres"
+import { drizzle as drizzleNode } from "drizzle-orm/node-postgres"
 import { config } from "dotenv"
 
 // // Read the environment variables
 config({ path: ".env" })
 
 // Connect to Vercel Postgres
-export const db = drizzle(sql)
+// export const db = drizzle(sql)
+
+// Connect to Vercel Postgres (via )
+export const db = drizzleNode(new Pool({ connectionString: process.env.DATABASE_URL }))
